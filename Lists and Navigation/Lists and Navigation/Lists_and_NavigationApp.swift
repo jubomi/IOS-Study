a7243d9611866043ab8d8775a6fd64a50ae695a1
jubomi <bomi050623@gmail.com>
2021-12-24 09:09:18 +0900

    [UPDATE] Lists and navigation
    Lists and Navigation

---
+++ Lists and Navigation/Lists and Navigation/Lists_and_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lists_and_NavigationApp.swift
+//  Lists and Navigation
+//
+//  Created by 주보미 on 2021/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Lists_and_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
